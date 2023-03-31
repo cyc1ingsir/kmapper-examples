f32f6c00413ea03203cec72a5780df51820ee90f
Meinhard Ritscher <unreachable@gmx.net>
2023-03-31 23:25:08 +0200

    Revert "Not working test to make mapping depending on the context"
    This reverts commit 6667ca70fdd76838c7456a1620440e6efee98924.

--- src/main/kotlin/com/syouth/kmapper/testload/mappers/recursiveTest/RecursiveTestMapper.kt
+++ src/main/kotlin/com/syouth/kmapper/testload/mappers/recursiveTest/RecursiveTestMapper.kt
@@ -32,34 +32,19 @@ interface RecursiveTestMapper {
 }
 */
 
-/*@Mapper
+@Mapper
 interface PartBDto2DomainMapper {
     fun map(dto: RecDtoPartB, @Bind relations: MutableList<AtoBRelationDomain>) : RecDomainPartB
 
     fun map(domain: RecDomainPartB, @Bind relations: MutableList<AtoBRelationDto>) : RecDtoPartB
-}*/
-
-interface RelationMapperHelper {
-
-    fun mapRelationMember(dto: RecDtoPartB): RecDomainPartB = mapWithList(dto, mutableListOf())
-    fun mapRelationMember(dto: RecDtoPartA): RecDomainPartA = mapWithList(dto, mutableListOf())
-    fun mapWithList(dto: RecDtoPartA, @Bind relations: MutableList<AtoBRelationDomain>) : RecDomainPartA
-    fun mapWithList(dto: RecDtoPartB, @Bind relations: MutableList<AtoBRelationDomain>) : RecDomainPartB
-
-}
-
-@Mapper
-interface PartBDto2DomainMapper  {
-    fun map(dto: RecDtoPartB) : RecDomainPartB
-
 }
 
 @Mapper
 interface PartADto2DomainMapper {
-    fun map(dto: RecDtoPartA) : RecDomainPartA
+    fun map(dto: RecDtoPartA, @Bind relations: MutableList<AtoBRelationDomain>) : RecDomainPartA
 }
 
 @Mapper
-interface RelationDto2DomainMapper: RelationMapperHelper {
-    fun map(dto: AtoBRelationDto) : AtoBRelationDomain
+interface RelationDto2DomainMapper {
+    fun map(dto: AtoBRelationDto, @Bind partA: RecDomainPartA, @Bind partB: RecDomainPartB) : AtoBRelationDomain
 }
