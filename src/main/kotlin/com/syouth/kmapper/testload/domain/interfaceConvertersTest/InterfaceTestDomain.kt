c02e4b21d125b2f3b6d0d365fb6ded259e040f1c
Meinhard Ritscher <unreachable@gmx.net>
2025-09-27 12:47:42 +0200

    Explain member naming

--- src/main/kotlin/com/syouth/kmapper/testload/domain/interfaceConvertersTest/InterfaceTestDomain.kt
+++ src/main/kotlin/com/syouth/kmapper/testload/domain/interfaceConvertersTest/InterfaceTestDomain.kt
@@ -3,6 +3,9 @@ package com.syouth.kmapper.testload.domain.interfaceConvertersTest
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
 
 data class InterfaceTestDomain(
+    // data type DTO -> To -> data type Domain
+    // e.g. floatDTO To IntDomain
+    //      bigDecimalDTO To StringDomain
     val floatToInt: Int,
     val doubleToFloat: Float?,
     val strToBigDecimal: BigDecimal,
--- src/main/kotlin/com/syouth/kmapper/testload/dto/interfaceConvertersTest/InterfaceTestDto.kt
+++ src/main/kotlin/com/syouth/kmapper/testload/dto/interfaceConvertersTest/InterfaceTestDto.kt
@@ -3,6 +3,9 @@ package com.syouth.kmapper.testload.dto.interfaceConvertersTest
 import com.ionspin.kotlin.bignum.decimal.BigDecimal
 
 data class InterfaceTestDto(
+    // data type DTO -> To -> data type Domain
+    // e.g. floatDTO To IntDomain
+    //      bigDecimalDTO To StringDomain
     val floatToInt: Float,
     val doubleToFloat: Double,
     val strToBigDecimal: String,
